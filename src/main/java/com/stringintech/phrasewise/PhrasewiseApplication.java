747c57e6775fdad469a3bc3eff32f89c7921388c
stringintech <stringintech@gmail.com>
2024-12-10 21:42:25 +0330

    Initialize

---
+++ src/main/java/com/stringintech/phrasewise/PhrasewiseApplication.java
@@ -0,0 +1,13 @@
+package com.stringintech.phrasewise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhrasewiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhrasewiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stringintech/phrasewise/PhrasewiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stringintech.phrasewise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhrasewiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
